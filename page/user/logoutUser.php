6ec4e24ba55bd871ee14ca505272776aaf454ed7
WillyRumapea <willyrumapea123jr@gmail.com>
2024-10-16 19:56:34 +0700

    logout style

--- page/user/logoutUser.php
+++ page/user/logoutUser.php
@@ -6,9 +6,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Logout</title>
+
+    <!-- css style -->
+     <link rel="stylesheet" href="../style/logout.css">
+    <!-- css style -->
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
+    <!-- fonts -->    
 </head>
 <body>
-    <a href="../../controller/logout.php">logout</a>
+    <div class="container-logout">
+        <a href="../../controller/logout.php">logout</a>
+    </div>
 </body>
 </html>
